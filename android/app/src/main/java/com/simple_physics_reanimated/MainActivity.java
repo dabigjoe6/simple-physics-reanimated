440890cdb12104dfe3f951ea1bee540277ac723c
Joseph <dabigjoe6@gmail.com>
2020-05-18 15:15:36 +0100

    Add react-native-gesture-handler

--- android/app/src/main/java/com/simple_physics_reanimated/MainActivity.java
+++ android/app/src/main/java/com/simple_physics_reanimated/MainActivity.java
@@ -1,6 +1,9 @@
 package com.simple_physics_reanimated;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+ import com.facebook.react.ReactRootView;
+ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "simple_physics_reanimated";
+  }
+
+    @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
