a1860ce93d252df26645ab89fd7b0bcc2525525b
Joseph <dabigjoe6@gmail.com>
2020-05-18 16:28:25 +0100

    Translate along x

--- App.js
+++ App.js
@@ -1,114 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Animated, { Value, event, cond, eq, set, add } from 'react-native-reanimated';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const styles = {
+	box: {
+		height: 100,
+		width: 100,
+		backgroundColor: '#018282',
+	},
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	}
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+function interaction(gestureTranslation, gestureState) {
+	const start = new Value(0);
+	const dragging = new Value(0);
+	const position = new Value(0);
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+	return cond(
+		eq(gestureState, State.ACTIVE),
+		[
+			cond(eq(dragging, 0), [set(dragging, 1), set(start, position)]),
+			set(position, add(start, gestureTranslation)),
+		],
+		[set(dragging, 0), position]
+	);
+}
 
-export default App;
+export default class App extends Component {
+	constructor(props) {
+		super(props)
+		
+		const gestureX = new Value(0);
+		const state = new Value(-1);
+
+		this._onGestureEvent = event([
+			{
+				nativeEvent: {
+					translationX: gestureX,
+					state: state
+				},
+			}
+		]);
+
+		this._transX = interaction(gestureX, state)
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<PanGestureHandler
+					onGestureEvent={this._onGestureEvent}
+					onGestureStateChange={this._onGestureEvent}
+				>
+					<Animated.View style={{
+						...styles.box,
+						transform: [{ translateX: this._transX }]
+					}} /> 
+				</PanGestureHandler>
+			</View>
+		)
+	}
+}
